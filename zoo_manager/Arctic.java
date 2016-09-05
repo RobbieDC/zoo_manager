13d73ba8209cc284cbdd64934d731ae028f4dad4
Robbie Campbell <rcampbell182@gmail.com>
2016-09-05 11:01:54 +0100

    tested arctible interface was correctly implemented on polarBear

--- zoo_manager/Arctic.java
+++ zoo_manager/Arctic.java
@@ -1,9 +1,15 @@
 package zoo_manager;
 
+import java.util.*;
+import behaviours.*;
+
 public class Arctic extends Enclosure {
+
+  ArrayList<Arcticable> occupants;
 
   public Arctic(int capacity) {
     super(capacity);
+    this.occupants = new ArrayList<Arcticable>();
   }
-  
+
 }
