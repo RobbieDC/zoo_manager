bce68fc0df5f8b09a23a0f1346687c4cdae6f6c9
Robbie Campbell <rcampbell182@gmail.com>
2016-09-06 11:39:07 +0100

    removed redundant zoo tests

--- zoo_manager_specs/ZooTest.java
+++ zoo_manager_specs/ZooTest.java
@@ -1,29 +1,19 @@
-// import static org.junit.Assert.*;
-// import org.junit.*;
-// import zoo_manager.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+import zoo_manager.*;
 
-// public class ZooTest {
+public class ZooTest {
 
-//   Zoo zoo;
+  Zoo zoo;
 
-//   @Before
-//   public void before() {
-//     zoo = new Zoo();
-//   }
+  @Before
+  public void before() {
+    zoo = new Zoo();
+  }
 
-  // @Test
-  // public void canGetEmpyEnclosureArrayList() {
-  //   assertEquals( <[]>, zoo.getEnclosures() );
-  // }
+  @Test
+  public void canGetEmpyEnclosureArrayList() {
+    assertEquals( 0, zoo.getEnclosures().size() );
+  }
 
-  // @Test
-  // public void startsWithNoEnclosures() {
-  //   assertEquals( 0, zoo.numEnclosures() );
-  // }
-
-  // @Test
-  // public void canAddEnclosure() {
-  //   assertEquals( 1, zoo.addEnclosure() );
-  // }
-
-// }
+}
